ca4e918d386edc0af7992b9385871609a6cc586c
alekss <anoviconoks@gmail.com>
2024-03-15 11:09:45 +0100

    Made skeleton for the app

--- src/GUI/MainController.java
+++ src/GUI/MainController.java
@@ -1,10 +1,22 @@
 package GUI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.util.Random;
 
 public class MainController {
 
+    @FXML
+    private Button Prev;
+    @FXML
+    private Button nextBtn;
+    @FXML
+    private Label imgname;
+
+    public void loadImage(ActionEvent actionEvent) {
+
+    }
 }
