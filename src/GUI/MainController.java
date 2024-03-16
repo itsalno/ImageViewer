5e08fd0b2a19eff670f12ba4e36d51b146e6ef72
alekss <anoviconoks@gmail.com>
2024-03-16 11:05:43 +0100

    Did a bit of styling and load image functionality

--- src/GUI/MainController.java
+++ src/GUI/MainController.java
@@ -2,13 +2,22 @@ package GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
 import java.util.Random;
+import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
 
+    public ImageView imagePlace;
     @FXML
     private Button Prev;
     @FXML
@@ -17,6 +26,22 @@ public class MainController {
     private Label imgname;
 
     public void loadImage(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+
+            Image image = new Image(file.toURI().toString());
+            imagePlace.setImage(image);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
